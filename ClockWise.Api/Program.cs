dd482f0442bcbecae10152c709c70efbb4377c0c
João Barreto <barretobit@gmail.com>
2025-02-15 23:49:24 +0100

    Deleting comments from Program.cs;

--- ClockWise.Api/Program.cs
+++ ClockWise.Api/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<ClockWiseDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ClockWiseConnection")));
 
@@ -25,7 +24,6 @@ builder.Services.AddAuthentication("Bearer")
         };
     });
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
