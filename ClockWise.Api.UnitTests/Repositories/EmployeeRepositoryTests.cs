f2ee9c78b2d80dbb2bff8fc5fc3917e82d19c54d
João Barreto <barretobit@gmail.com>
2025-02-19 23:57:35 +0100

    Creating UnitTests for Employee and EmployeeType repositories;

---
+++ ClockWise.Api.UnitTests/Repositories/EmployeeRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClockWise.Api.Data;
+using ClockWise.Api.Repositories;
+
+namespace ClockWise.Api.UnitTests.Repositories
+{
+    public class EmployeeRepositoryTests
+    {
+        private readonly ClockWiseDbContext _dbContext;
+        private readonly EmployeeRepository _repository;
+
+        public EmployeeRepositoryTests()
+        {
+
+        }
+    }
+}
---
+++ ClockWise.Api.UnitTests/Repositories/EmployeeTypeRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using ClockWise.Api.Data;
+using ClockWise.Api.Models;
+using ClockWise.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClockWise.Api.UnitTests.Repositories
+{
+    public class EmployeeTypeRepositoryTests
+    {
+        private readonly ClockWiseDbContext _dbContext;
+        private readonly EmployeeTypeRepository _repository;
+
+        public EmployeeTypeRepositoryTests()
+        {
+
+        }
+    }
+}
